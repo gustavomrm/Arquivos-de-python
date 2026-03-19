b016a1fbbfa483058af7b9f7cf4c819d72e59993
gustavomrm <gustavo.mrm7@gmail.com>
2026-03-19 12:13:20 -0300

    Adicionando ex9.py

---
+++ ex9.py
@@ -0,0 +1,7 @@
+hora = float(input("Qual horario você gostaria de converter? "))
+
+valor_minuto = (hora * 60)
+print(f'O valor de horas em minutos é: {valor_minuto}')
+
+valor_segundos = (hora * 3600)
+print(f'O valor de horas em segundos é: {valor_segundos}')
